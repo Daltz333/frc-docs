d15357980e9584910bb07f221ee9f19f2daf5266
Daltz333 <daltzsmith@gmail.com>
2021-01-09 03:25:55 -0500

    frc-docs furo

--- source/conf.py
+++ source/conf.py
@@ -144,7 +144,7 @@ mathjax_path = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -153,15 +153,6 @@ html_static_path = ['_static']
 
 # Sidebar logo
 html_logo = "assets/wpilibDocsLogo.png"
-
-# URL favicon
-html_favicon = "assets/FIRSTicon_RGB_withTM.ico"
-
-html_theme_options = {
-	'collapse_navigation': True,
-	'sticky_navigation': False,
-	'titles_only': True
-}
 
 user_options = [
         ('warning-is-error', True),
